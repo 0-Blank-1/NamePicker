eb3acf6669d55984cd2b63ab6169f4209a8e87c4
0-Blank-1 <sauravthapa2565@gmail.com>
2025-12-14 15:34:02 +0545

    Create users.js

---
+++ users.js
@@ -0,0 +1,14 @@
+// WARNING: This file is publicly accessible and readable by anyone who visits your website.
+// Do not use this method for sensitive data.
+
+const AUTHORIZED_USERS = {
+    // Format: "Username": "password"
+    "Saurav": "admin",
+    "Ashruk": "pass123",
+    "Arushi": "secret",
+    "TestUser": "test"
+    // Add more users here:
+    // "NewUser": "their_password",
+};
+
+// The main script will access this AUTHORIZED_USERS object.
